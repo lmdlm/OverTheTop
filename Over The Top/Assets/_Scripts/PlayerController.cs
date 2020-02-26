daf20421f3b52601cb81bdd8845b833c76526f4e
alexh <g00363430@gmit.ie>
2020-02-26 10:34:32 +0000

    Start of the project
    Camera, player background added

---
+++ Over The Top/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // == public fields ==
+
+    // == private fields ==
+    private Rigidbody2D rb;
+
+    private Camera gameCamera;
+
+    [SerializeField] private float speed = 7.0f;
+    [SerializeField] private float angle = 0.0f;
+
+    // == private methods ==
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // add hMovement
+        // if the player presses the up arrow, then move
+        float vMovement = Input.GetAxis("Vertical");
+        float hMovement = Input.GetAxis("Horizontal");
+        // apply a force, get the player moving.
+        rb.velocity = new Vector2(hMovement * speed, vMovement * speed);
+        //rotation left & right
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    while(angle < 1.0f)
+        //    {
+        //        // Clockwise
+        //        transform.Rotate(0.0f, angle, 0.0f);
+        //        angle += 0.2f;
+        //    }
+        //}
+
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    while (angle > -1.0f)
+        //    {
+        //        // Counter-clockwise
+        //        transform.Rotate(0.0f, angle, 0.0f);
+        //        angle -= 0.2f;
+        //    }
+        //}
+        // keep the player on the screen
+        // float xValue = Mathf.clamp(value, min, max)
+        // rb.position.x 
+        float yValue = Mathf.Clamp(rb.position.y, 0.0f, 7.0f);
+        float xValue = Mathf.Clamp(rb.position.x, -6.5f, 6.5f);
+
+        rb.position = new Vector2(xValue, yValue);
+    }
+}
---
+++ Over The Top/Assets/_Scripts/WeaponController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    // == private fields ==
+    [SerializeField] private float bulletSpeed = 6.0f;
+    [SerializeField] private Bullet bulletPrefab;
+    [SerializeField] private float firingCoroutineSpeed = 0.1f;
+
+    private GameObject bulletParent;
+
+    private Coroutine firingCoroutine; //pointer to the coroutine - need this to stop firing
+
+    // == private methods
+    private void Start()
+    {
+        bulletParent = GameObject.Find("BulletParent");
+        if (!bulletParent)
+        {
+            bulletParent = new GameObject("BulletParent");
+        }
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //set up a cooroutine to fire the bullets
+            firingCoroutine = StartCoroutine(FireCoroutine());
+            // FireBullet();
+        }
+        //stop firing
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            StopCoroutine(firingCoroutine);
+        }
+    }
+
+    //coroutine must be of type IEnumerator
+    private IEnumerator FireCoroutine()
+    {
+        while (true)
+        {
+            //generate bullets
+            Bullet bullet = Instantiate(bulletPrefab, bulletParent.transform);
+            bullet.transform.position = transform.position;
+            Rigidbody2D rbb = bullet.GetComponent<Rigidbody2D>();
+            rbb.velocity = Vector2.up * bulletSpeed;
+            //yield return causes the method to pasue
+            //sleep()
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+
+    private void FireBullet()
+    {
+        // instantiate bullet
+        Bullet bullet = Instantiate(bulletPrefab, bulletParent.transform);
+        // give it the same position as the player
+        bullet.transform.position = transform.position;
+        // give it velocity and move right
+        Rigidbody2D rbb = bullet.GetComponent<Rigidbody2D>();
+        //rbb.velocity = new Vector2(1 * speed, 0);
+        rbb.velocity = Vector2.right * bulletSpeed;
+    }
+}
