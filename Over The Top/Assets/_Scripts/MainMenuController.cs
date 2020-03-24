d7e2d390a4d6691f92c2c0c2a225e775483c3dbe
alexh <g00363430@gmit.ie>
2020-03-24 09:09:35 +0000

    Music and sounds added
    Added music for the game. Shot sound clip added for when firing bullets

---
+++ Over The Top/Assets/_Scripts/MainMenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//add this for scene management
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    //handle button events
+    //start button, options button
+    //use SceneManager object to load and unload
+    //all methods are static
+    //SceneLoaded, SceneUnloaded, SceneChange for custom logic
+    //LoadSceneAsync to load a new scene
+    //SceneManager is in UnityEngine.SceneManagement library
+
+    //OnClick Event Handler
+    public void Start_OnClick()
+    {
+        SceneManager.LoadSceneAsync("Level1");
+    }
+
+    public void Options_OnClick()
+    {
+        SceneManager.LoadSceneAsync("OptionsMenu", LoadSceneMode.Additive);
+    }
+}
--- Over The Top/Assets/_Scripts/WeaponController.cs
+++ Over The Top/Assets/_Scripts/WeaponController.cs
@@ -3,18 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class WeaponController : MonoBehaviour
 {
     // == private fields ==
     [SerializeField] private float bulletSpeed = 6.0f;
     [SerializeField] private Bullet bulletPrefab;
-    [SerializeField] private float firingCoroutineSpeed = 0.1f;
+    [SerializeField] private float firingRate = 0.25f;
+    [SerializeField] private AudioClip shootClip;
+    [SerializeField] [Range(0f, 1.0f)] private float shootVolume = 0.5f;
 
+    private Coroutine firingCoroutine;
     private GameObject bulletParent;
+    private AudioSource audioSource;
 
-    private Coroutine firingCoroutine; //pointer to the coroutine - need this to stop firing
-
-    // == private methods
+    // == private methods ==
     private void Start()
     {
         bulletParent = GameObject.Find("BulletParent");
@@ -22,48 +25,54 @@ public class WeaponController : MonoBehaviour
         {
             bulletParent = new GameObject("BulletParent");
         }
-
+        audioSource = GetComponent<AudioSource>();
     }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            //set up a cooroutine to fire the bullets
+            // one way to fire
+            //FireBullet();
+            // implement a coroutine to fire
             firingCoroutine = StartCoroutine(FireCoroutine());
-            // FireBullet();
         }
-        //stop firing
         if (Input.GetKeyUp(KeyCode.Space))
         {
+            //StopAllCoroutines();    // not good, sledgehammer approach
             StopCoroutine(firingCoroutine);
         }
     }
 
-    //coroutine must be of type IEnumerator
+    // coroutine returns an IEnumerator type
     private IEnumerator FireCoroutine()
     {
         while (true)
         {
-            //generate bullets
+            // create a bullet
             Bullet bullet = Instantiate(bulletPrefab, bulletParent.transform);
             bullet.transform.position = transform.position;
+            // play sound - AudioClip, Volume between 0 and 1
+            //AudioSource.PlayClipAtPoint(shootClip, Camera.main.transform.position,shootVolume);
+            // use a local AudioSource
+            audioSource.PlayOneShot(shootClip, shootVolume);
+
             Rigidbody2D rbb = bullet.GetComponent<Rigidbody2D>();
             rbb.velocity = Vector2.up * bulletSpeed;
-            //yield return causes the method to pasue
-            //sleep()
-            yield return new WaitForSeconds(0.3f);
+            // sleep for short time
+            yield return new WaitForSeconds(firingRate); // pick a number!!!
         }
     }
 
-    private void FireBullet()
-    {
-        // instantiate bullet
-        Bullet bullet = Instantiate(bulletPrefab, bulletParent.transform);
-        // give it the same position as the player
-        bullet.transform.position = transform.position;
-        // give it velocity and move right
-        Rigidbody2D rbb = bullet.GetComponent<Rigidbody2D>();
-        //rbb.velocity = new Vector2(1 * speed, 0);
-        rbb.velocity = Vector2.right * bulletSpeed;
-    }
+    //private void FireBullet()
+    //{
+    //    // instantiate bullet
+    //    Bullet bullet = Instantiate(bulletPrefab, bulletParent.transform);
+    //    // give it the same position as the player
+    //    bullet.transform.position = transform.position;
+    //    // give it velocity and move right
+    //    Rigidbody2D rbb = bullet.GetComponent<Rigidbody2D>();
+    //    //rbb.velocity = new Vector2(1 * speed, 0);
+    //    rbb.velocity = Vector2.right * bulletSpeed;
+    //}
 }
